37d1da2a594cd4ab16d3aa941f08701c171bc52c
Zhenboro <zhenboro@outlook.com>
2022-06-12 00:18:00 -0300

    Create ArbitraCode.vb
    - Ejemplo de un código para broArbitra

---
+++ broArbitra/ArbitraCode.vb
@@ -0,0 +1,22 @@
+Public Class broCodeProvider
+    Public Sub New()
+
+    End Sub
+    ' This function is called ONE TIME.
+    ' Like a New() Or a Load
+    ' A parameter can be passed. Idk about what. You choice.
+    Public Function Initiali(Optional ByVal initParam As String = Nothing) As String
+        ' Do some... idk.
+        '   A mean, call a sub o function. Idk.
+        Return "" 'A return must be returned. You choice.
+    End Function
+
+    Public Function Arbitra(Optional ByVal Param As String = Nothing) As String
+        ' Do some... idk.
+        '   A mean, call a sub o function. Idk.
+        Return "" 'A return must be returned. You choice.
+    End Function
+
+    ' Your code here and beyond!
+
+End Class
